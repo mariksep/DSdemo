62dd86fe472491c6c5082417c041487f7c6dc9d1
maris <mari.seppanen4@metropolia.fi>
2020-02-20 09:57:30 +0200

    Merge branch 'master' of C:\Users\maris\Desktop\DSdemo with conflicts.

--- src/modules/network.js
+++ src/modules/network.js
@@ -10,18 +10,4 @@ const getCourses = async () => {
   }
 };
 
-/**
- *  Fetch current weather-data (in Myyrmäki) and convert it to json
- */
-const getWeatherData = async () => {
-  const weatherURL = `http://api.openweathermap.org/data/2.5/weather?zip=01600,fi&appid=c13ea6073dd0700495786ba6f93af408&units=metric`;
-  try {
-    const response = await fetch(weatherURL);
-    const json = await response.json();
-    return json;
-  } catch (error) {
-    console.log('Error in fetching weather data', error);
-  }
-};
-
-export { getCourses, getWeatherData };
+export{ getCourses};
