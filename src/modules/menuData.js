86e982cbd088b7b5968aa512541f40c06938a44e
maris <mari.seppanen4@metropolia.fi>
2020-02-26 12:20:28 +0200

    carousel

--- src/modules/menuData.js
+++ src/modules/menuData.js
@@ -2,7 +2,7 @@ import {getCourses} from './network';
 
 
 /**
- * Display current lunch data in HTML
+ * Display current lunch data in HTML in fin
  */
 let title = document.querySelector(".titleM");
 const getInit = () =>{
@@ -25,11 +25,36 @@ let menu = document.querySelector(".menu");
   };
   getCoursesSodexo();
 };
+/**
+ * Display current lunch data in HTML in En
+ */
+let titleE = document.querySelector(".titleE");
+const getInitE = () =>{
+let menu = document.querySelector(".menuE");
+  const getCoursesSodexo =async () => {
+    try{
+      const data = await getCourses();
+      const objl = Object.keys(data.courses).length;
+      titleE.innerHTML= data.meta.ref_title;
+        for(let i= 1; i<=objl; i++){
+          let p = document.createElement("p");
+          p.innerHTML=  data.courses[i].title_en  +" " +data.courses[i].properties +" " +data.courses[i].price ;
+        menu.appendChild(p);
+        }
+
+    }catch (e) {
+       console.log('error' + e);
+     }
+
+  };
+  getCoursesSodexo();
+};
 
 
 
 
 
-const menuData = {getInit};
+
+const menuData = {getInit, getInitE};
 
 export default menuData;
