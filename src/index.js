ba4687269eda8136b9f23d600edceac523962622
maris <mari.seppanen4@metropolia.fi>
2020-02-28 11:01:11 +0200

    css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import menuData from "./modules/menuData";
 import WeatherData from "./modules/weatherData";
 import transitData from "./modules/transitData";
 import NewsFeedData from "./modules/newsFeedData";
-
+/*
 const viewCarousel = (activeViewIndex, duration) => {
   const views = document.getElementsByClassName('main_content');
   for (const view of views) {
@@ -16,8 +16,7 @@ const viewCarousel = (activeViewIndex, duration) => {
   setTimeout(() => viewCarousel(nextView, duration), duration * 1000);
 };
 viewCarousel(0, 10);
-
-
+*/
 menuData.getInit('fin');
 menuData.getInit('en');
 
@@ -27,8 +26,11 @@ setInterval(WeatherData.displayWeatherData, 60*60*1000);
 
 transitData.getTransitData('4150296');
 transitData.getTransitData('4150201');
-setInterval(transitData.getTransitDataV, 60*1000);
-setInterval(transitData.getTransitDataO, 60*1000);
+transitData.getTransitData('4150264');
+transitData.getTransitData('4150266');
+
+setInterval(transitData.getTransitData, 60*1000);
+
 
 
 NewsFeedData.displayNewsFeed('finnish');
