d267ca15a3aabfd6fb97d81bd21d8048343076f9
maris <mari.seppanen4@metropolia.fi>
2020-03-02 14:12:37 +0200

    css

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import NewsFeedData from "./modules/newsFeedData";
 
 let weatherInterval;
 let tarnsitInterval;
-/*
+
 const viewCarousel = (activeViewIndex, duration) => {
   const views = document.getElementsByClassName('main_content');
   for (const view of views) {
@@ -19,7 +19,7 @@ const viewCarousel = (activeViewIndex, duration) => {
   setTimeout(() => viewCarousel(nextView, duration), duration * 1000);
 };
 viewCarousel(0, 10);
-*/
+
 
 const pMyllypuro = document.querySelector(".myllypuro");
 const pMyyrmaki = document.querySelector(".myyrmäki");
